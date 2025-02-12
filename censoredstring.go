adcfe8a3da0cf83e6abdbc8aed51fa3ea5b0484c
Niels Henrik Hagen <nhhagen@gmail.com>
2025-02-12 09:50:55 +0100

    docs: Use proper examples

--- censoredstring.go
+++ censoredstring.go
@@ -10,6 +10,7 @@ package masker
 import (
 	"encoding"
 	"fmt"
+	"log/slog"
 )
 
 // CensoredText is the text displayed instead of protected values when they are masked.
@@ -23,14 +24,32 @@ func (s CensoredString) MarshalText() ([]byte, error) {
 	return []byte(s.String()), nil
 }
 
-// String returns the native format for a CensoredString value, which will invariably be the CensoredString constant.
+// String returns the native format for a CensoredString value, which will invariably be the value of the CensoredText constant.
 func (s CensoredString) String() string {
 	return CensoredText
 }
 
-// GoString returns the Go syntax for a CensoredString value, which will invariably be the Go syntax for the CensoredString constant.
+// GoString returns the Go syntax for a CensoredString value, which will invariably be the Go syntax for the CensoredText constant.
 func (s CensoredString) GoString() string {
 	return fmt.Sprintf("%#v", s.String())
+}
+
+// Format formats the CensoredString according to the verb, which will
+// invariably be the CensoredString constant.
+func (s CensoredString) Format(f fmt.State, verb rune) {
+	var value string
+	if verb == 'v' && f.Flag('#') {
+		value = s.GoString()
+	} else {
+		value = s.String()
+	}
+	f.Write([]byte(value)) //nolint:errcheck
+}
+
+// LogValue converts the CensoredString into a slog.Value, which will
+// invariably contain the value of the CensoredString constant.
+func (s CensoredString) LogValue() slog.Value {
+	return slog.StringValue(s.String())
 }
 
 // UnmaskString returns the underlying protected string of a CensoredString value.
@@ -42,5 +61,7 @@ var (
 	_ encoding.TextMarshaler = (*CensoredString)(nil)
 	_ fmt.Stringer           = (*CensoredString)(nil)
 	_ fmt.GoStringer         = (*CensoredString)(nil)
+	_ fmt.Formatter          = (*CensoredString)(nil)
 	_ StringUnmasker         = (*CensoredString)(nil)
+	_ slog.LogValuer         = (*CensoredString)(nil)
 )
