ae9ed7729aa0f3c7682e162494e7a9a5bc61a73b
Niels Henrik Hagen <nhhagen@gmail.com>
2025-02-24 14:23:08 +0100

    Remove separate change

--- censoredstring.go
+++ censoredstring.go
@@ -10,7 +10,6 @@ package masker
 import (
 	"encoding"
 	"fmt"
-	"log/slog"
 )
 
 // CensoredText is the text displayed instead of protected values when they are masked.
@@ -34,24 +33,6 @@ func (s CensoredString) GoString() string {
 	return fmt.Sprintf("%#v", s.String())
 }
 
-// Format formats the CensoredString according to the verb, which will
-// invariably be the CensoredString constant.
-func (s CensoredString) Format(f fmt.State, verb rune) {
-	var value string
-	if verb == 'v' && f.Flag('#') {
-		value = s.GoString()
-	} else {
-		value = s.String()
-	}
-	f.Write([]byte(value)) //nolint:errcheck
-}
-
-// LogValue converts the CensoredString into a slog.Value, which will
-// invariably contain the value of the CensoredString constant.
-func (s CensoredString) LogValue() slog.Value {
-	return slog.StringValue(s.String())
-}
-
 // UnmaskString returns the underlying protected string of a CensoredString value.
 func (s CensoredString) UnmaskString() string {
 	return string(s)
@@ -61,7 +42,5 @@ var (
 	_ encoding.TextMarshaler = (*CensoredString)(nil)
 	_ fmt.Stringer           = (*CensoredString)(nil)
 	_ fmt.GoStringer         = (*CensoredString)(nil)
-	_ fmt.Formatter          = (*CensoredString)(nil)
 	_ StringUnmasker         = (*CensoredString)(nil)
-	_ slog.LogValuer         = (*CensoredString)(nil)
 )
